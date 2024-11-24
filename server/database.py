42524750e50b9c690eaf74c49a9c1928ff675250
artyom <batmanov.2016@mail.ru>
2024-11-24 15:10:45 +0300

    изменил порты

--- server/database.py
+++ server/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@localhost/twitter"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@db:5432/twitter"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
